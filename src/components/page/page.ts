d84aaf871b466e05209ac166ecbda9266ae26ff6
lsg <tkdrl1500@gmail.com>
2021-11-04 17:39:01 +0900

    [Fix] 중복된 코드 개선 하기 위해 BaseComponent 클래스 생성

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,18 +1,8 @@
+import { BaseComponent } from '../component.js';
+
 // PageComponents에는 페이지에 대한 부모 컨테이너에 대한 요소들이 있으면 된다.
-export class PageComponent {
-  private element: HTMLUListElement;
-
+export class PageComponent extends BaseComponent<HTMLUListElement> {
   constructor() {
-    this.element = document.createElement('ul');
-    this.element.setAttribute('class', 'page');
-    this.element.textContent = 'This is PageComponent';
-  }
-
-  // attchTo라는 외부에서 사용할 수 있는 API를 만들었다.
-  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-    // InsertPosition은 아무것도 전달하지 않으면 기본적으로 afterbegin이다
-
-    //insertAdjacentElement는 특정 위치에서 노드를 삽입한다.
-    parent.insertAdjacentElement(position, this.element);
+    super(`<ul class='page'>This is PageComponent!</ul>`);
   }
 }
