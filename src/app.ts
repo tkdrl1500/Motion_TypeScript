ec0d43850991d2b8ebdf264756c4dd1f31bc3ee7
lsg <tkdrl1500@gmail.com>
2021-11-04 15:05:33 +0900

    [Add] 레이아웃과 Page 컴포넌트 구현

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import { PageComponent } from './components/page.js';
+
+class App {
+  private readonly page: PageComponent;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+  }
+}
+
+//! as HTMLElement <- null이 아니고 HTMLElement라고 해줬다
+// 어플리케이션이 시작되면 새로운 App 클래스를 만들어서 루트에는 document에 querySelector를 이용해서
+// Dom요수에 있는 document의 요소를 받아와서 그곳에 페이지를 추가해 주었다.
+new App(document.querySelector('.document')! as HTMLElement);
---
+++ src/components/page.ts
@@ -0,0 +1,23 @@
+import { ImageComponent } from './image.js';
+import { NoteComponent } from './note.js';
+import { TodoComponent } from './todo.js';
+import { VideoComponent } from './video.js';
+
+// PageComponents에는 페이지에 대한 부모 컨테이너에 대한 요소들이 있으면 된다.
+export class PageComponent {
+  private element: HTMLUListElement;
+
+  constructor() {
+    this.element = document.createElement('ul');
+    this.element.setAttribute('class', 'page');
+    this.element.textContent = 'This is PageComponent';
+  }
+
+  // attchTo라는 외부에서 사용할 수 있는 API를 만들었다.
+  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+    // InsertPosition은 아무것도 전달하지 않으면 기본적으로 afterbegin이다
+
+    //insertAdjacentElement는 특정 위치에서 노드를 삽입한다.
+    parent.insertAdjacentElement(position, this.element);
+  }
+}
