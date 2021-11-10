5cb5a8e8fa3a3cc577808ee6bd7daca9108fda6a
lsg <tkdrl1500@gmail.com>
2021-11-10 14:02:24 +0900

    [Add] Drag & Drop 드래깅 이벤트 구현 및 UI 완성

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -6,10 +6,18 @@ export interface Composable {
 
 // 아무것도 전달 받지 않고 아무것도 리턴하지 않고 단지 닫쳤다고 알려주기만 하는 콜백함수이다
 type OnCloseListener = () => void;
-
+type DragState = 'start' | 'stop' | 'enter' | 'leave';
+type OnDragStateListener<T extends Component> = (
+  target: T,
+  state: DragState
+) => void;
 //PageItemComponent는 setOnCloseListener는 꼭 구현해야 된다 close가 가능해야되기 때문이다
 interface SectionContainer extends Component, Composable {
   setOnCloseListener(listener: OnCloseListener): void;
+  setOnDragStateListener(listener: OnDragStateListener<SectionContainer>): void;
+  muteChildren(state: 'mute' | 'unmute'): void;
+  getBoundingRect(): DOMRect;
+  onDropped(): void;
 }
 
 type SectionContainerConstructor = {
@@ -24,6 +32,7 @@ export class PageItemComponent
   //PageItemComponent는 어디에 속해 있는지 모르기 때문에 외부로부터 전달받은 콜백함수를 호출해줘야 된다
   // 외부로 부터 받은 콜백함수를 closeListener 변수에 저장
   private closeListener?: OnCloseListener;
+  private dragStateListener?: OnDragStateListener<PageItemComponent>;
   constructor() {
     super(`<li draggable="true" class="page-item">
             <section class="page-item__body"></section>
@@ -43,13 +52,38 @@ export class PageItemComponent
     this.element.addEventListener('dragend', (event: DragEvent) => {
       this.onDragEnd(event);
     });
+    this.element.addEventListener('dragenter', (event: DragEvent) => {
+      this.onDragEnter(event);
+    });
+    this.element.addEventListener('dragleave', (event: DragEvent) => {
+      this.onDragLeave(event);
+    });
   }
 
-  onDragStart(event: DragEvent) {
-    console.log('dragstart', event);
+  // event -> _ : 전달 받은 이벤트를 사용하지 않아서 받지 않게 처리 했다
+  onDragStart(_: DragEvent) {
+    this.notifyDragObservers('start');
+    this.element.classList.add('lifted');
   }
-  onDragEnd(event: DragEvent) {
-    console.log('dragend', event);
+  onDragEnd(_: DragEvent) {
+    this.notifyDragObservers('stop');
+    this.element.classList.remove('lifted');
+  }
+  onDragEnter(_: DragEvent) {
+    this.notifyDragObservers('enter');
+    this.element.classList.add('drop-area');
+  }
+  onDragLeave(_: DragEvent) {
+    this.notifyDragObservers('leave');
+    this.element.classList.remove('drop-area');
+  }
+
+  onDropped() {
+    this.element.classList.remove('drop-area');
+  }
+
+  notifyDragObservers(state: DragState) {
+    this.dragStateListener && this.dragStateListener(this, state);
   }
 
   addChild(child: Component) {
@@ -62,6 +96,22 @@ export class PageItemComponent
   setOnCloseListener(listener: OnCloseListener) {
     this.closeListener = listener;
   }
+  //Drag
+  setOnDragStateListener(listener: OnDragStateListener<PageItemComponent>) {
+    this.dragStateListener = listener;
+  }
+
+  muteChildren(state: 'mute' | 'unmute') {
+    if (state === 'mute') {
+      this.element.classList.add('mute-children');
+    } else {
+      this.element.classList.remove('mute-children');
+    }
+  }
+
+  getBoundingRect(): DOMRect {
+    return this.element.getBoundingClientRect();
+  }
 }
 
 // PageComponents에는 페이지에 대한 부모 컨테이너에 대한 요소들이 있으면 된다.
@@ -69,6 +119,11 @@ export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable
 {
+  //새로운 Set이라는 자료구조를 만들었습니다. Map에는 중복되는 데이터를 가질 수 있지만 Set이라고 하는 것는 중복된 데이터를 가질 수 없는 자료구조 입니다
+  private children = new Set<SectionContainer>();
+  private dragTarget?: SectionContainer;
+  private dropTarget?: SectionContainer;
+
   constructor(private pageItemConstructor: SectionContainerConstructor) {
     super(`<ul class='page'></ul>`);
 
@@ -77,17 +132,32 @@ export class PageComponent
       this.onDragOver(event);
     });
     this.element.addEventListener('drop', (event: DragEvent) => {
-      this.onDrag(event);
+      this.onDrop(event);
     });
   }
 
   onDragOver(event: DragEvent) {
     event.preventDefault();
-    console.log('dragover', event);
+    // console.log('dragover', event);
   }
-  onDrag(event: DragEvent) {
+  onDrop(event: DragEvent) {
     event.preventDefault();
-    console.log('drag', event);
+    //여기서 위치를 바꿔주면 됩니다
+    //dropTarget이 undefined면 리턴
+    if (!this.dropTarget) {
+      return;
+    }
+    // 똑같은 아이템을 드래그와 드랍을 한번에 할 수 없기에 확인 해줍니다
+    if (this.dragTarget && this.dragTarget !== this.dropTarget) {
+      const dropY = event.clientY;
+      const srcElement = this.dragTarget.getBoundingRect();
+      this.dragTarget.removeFrom(this.element);
+      this.dropTarget.attach(
+        this.dragTarget,
+        dropY < srcElement.y ? 'beforebegin' : 'afterend'
+      );
+    }
+    this.dropTarget.onDropped();
   }
 
   addChild(section: Component) {
@@ -96,6 +166,35 @@ export class PageComponent
     item.attachTo(this.element, 'beforeend');
     item.setOnCloseListener(() => {
       item.removeFrom(this.element);
+      this.children.delete(item);
+    });
+    this.children.add(item);
+    item.setOnDragStateListener(
+      (target: SectionContainer, state: DragState) => {
+        switch (state) {
+          case 'start':
+            this.dragTarget = target;
+            this.updateSections('mute');
+            break;
+          case 'stop':
+            this.updateSections('unmute');
+            this.dragTarget = undefined;
+            break;
+          case 'enter':
+            this.dropTarget = target;
+            break;
+          case 'leave':
+            this.dropTarget = undefined;
+            break;
+          default:
+            throw new Error(`unsupported state: ${state}`);
+        }
+      }
+    );
+  }
+  private updateSections(state: 'mute' | 'unmute') {
+    this.children.forEach((section: SectionContainer) => {
+      section.muteChildren(state);
     });
   }
 }
